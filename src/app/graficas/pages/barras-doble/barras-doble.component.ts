aaa6c2dc2bca20edbdd33f48b8188503896cc209
Martín <magadiflo@gmail.com>
2022-01-21 13:25:53 -0500

    Añadir flexibilidad a nuestro componente personalizado

--- src/app/graficas/pages/barras-doble/barras-doble.component.ts
+++ src/app/graficas/pages/barras-doble/barras-doble.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ChartDataset } from 'chart.js';
 
 @Component({
   selector: 'app-barras-doble',
@@ -7,6 +8,16 @@ import { Component, OnInit } from '@angular/core';
   ]
 })
 export class BarrasDobleComponent implements OnInit {
+
+  proveedoresLabels: string[] = ['2021', '2022', '2023', '2024', '2025'];
+  productoData: ChartDataset<"bar", number[]>[] = [
+    { data: [50, 100, 500, 300, 900], label: 'Carros', backgroundColor: 'blue' },
+  ];
+
+  consumidoresLabels: string[] = ['2021', '2022', '2023', '2024', '2025'];
+  consumidorData: ChartDataset<"bar", number[]>[] = [
+    { data: [50, 80, 30, 100, 50], label: 'Licor', backgroundColor: 'red' },
+  ];
 
   constructor() { }
 
