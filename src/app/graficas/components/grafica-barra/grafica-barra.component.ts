2f84e5aa6b4dde462b9999e00160edce3aaad2c8
Martín <magadiflo@gmail.com>
2022-01-21 12:50:30 -0500

    Componente personalizado para mostrar gráficos

--- src/app/graficas/components/grafica-barra/grafica-barra.component.ts
+++ src/app/graficas/components/grafica-barra/grafica-barra.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+
+import { ChartConfiguration, ChartData, ChartType } from 'chart.js';
+import { BaseChartDirective } from 'ng2-charts';
 
 @Component({
   selector: 'app-grafica-barra',
@@ -8,9 +11,60 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GraficaBarraComponent implements OnInit {
 
+  @ViewChild(BaseChartDirective) chart: BaseChartDirective | undefined;
+
+  public barChartOptions: ChartConfiguration['options'] = {
+    responsive: true,
+  };
+  public barChartType: ChartType = 'bar';
+
+  public barChartData: ChartData<'bar'> = {
+    labels: ['2020', '2021', '2022', '2023', '2024', '2025', '2026'],
+    datasets: [
+      { data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A', backgroundColor: '#32a88f', hoverBackgroundColor: '#091eb8' },
+      { data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B', backgroundColor: '#a83265', hoverBackgroundColor: '#091eb8' },
+      { data: [10, 15, 20, 25, 30, 35, 40], label: 'Series C', backgroundColor: '#afb809', hoverBackgroundColor: '#091eb8' },
+    ]
+  };
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  public randomize(): void {
+    // Only Change 3 values
+    this.barChartData.datasets[0].data = [
+      Math.round(Math.random() * 100),
+      Math.round(Math.random() * 100),
+      Math.round(Math.random() * 100),
+      Math.round(Math.random() * 100),
+      Math.round(Math.random() * 100),
+      Math.round(Math.random() * 100),
+      Math.round(Math.random() * 100),
+    ];
+
+    this.barChartData.datasets[1].data = [
+      Math.round(Math.random() * 100),
+      Math.round(Math.random() * 100),
+      Math.round(Math.random() * 100),
+      Math.round(Math.random() * 100),
+      Math.round(Math.random() * 100),
+      Math.round(Math.random() * 100),
+      Math.round(Math.random() * 100),
+    ];
+
+    this.barChartData.datasets[2].data = [
+      Math.round(Math.random() * 100),
+      Math.round(Math.random() * 100),
+      Math.round(Math.random() * 100),
+      Math.round(Math.random() * 100),
+      Math.round(Math.random() * 100),
+      Math.round(Math.random() * 100),
+      Math.round(Math.random() * 100),
+    ];
+
+    this.chart?.update();
   }
 
 }
--- src/app/graficas/pages/barras/barras.component.ts
+++ src/app/graficas/pages/barras/barras.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 
-import { ChartConfiguration, ChartData, ChartEvent, ChartType } from 'chart.js';
+import { ChartConfiguration, ChartData, ChartType } from 'chart.js';
 import { BaseChartDirective } from 'ng2-charts';
 
 @Component({
