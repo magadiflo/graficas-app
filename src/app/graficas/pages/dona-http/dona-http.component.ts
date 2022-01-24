2d311f93039ecb5ebb9d913f1e35a89965ab32a6
Martín <magadiflo@gmail.com>
2022-01-24 10:44:27 -0500

    Mostrar la información en la gráfica

--- src/app/graficas/pages/dona-http/dona-http.component.ts
+++ src/app/graficas/pages/dona-http/dona-http.component.ts
@@ -1,6 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { ChartData, ChartType } from 'chart.js';
 
 import { GraficasService } from '../../services/graficas.service';
+
+interface Data {
+  data: number[],
+}
 
 @Component({
   selector: 'app-dona-http',
@@ -10,12 +15,28 @@ import { GraficasService } from '../../services/graficas.service';
 })
 export class DonaHttpComponent implements OnInit {
 
+  public doughnutChartLabels: string[] = [];
+  public datasets: Data[] = [];
+  public doughnutChartData!: ChartData<'doughnut'>;
+  public doughnutChartType: ChartType = 'doughnut';
+
   constructor(private graficasService: GraficasService) { }
 
   ngOnInit(): void {
     this.graficasService.getUsuariosRedesSociales()
       .subscribe(data => {
-        console.log(data);  
+        const labels = Object.keys(data);
+        const values = Object.values(data);
+
+        this.doughnutChartLabels = labels;
+        this.datasets.push({ data: values });
+
+        // console.log(this.doughnutChartLabels);
+        // console.log(this.datasets);
+        this.doughnutChartData = {
+          labels: this.doughnutChartLabels,
+          datasets: this.datasets,
+        }
       });
   }
 
